55cd42dc512da079fcd2470217ddf5f76aca7596
Little-Bird <Little-Bird>
2017-07-30 02:14:09 +0800

    create sharpBase By Bird

---
+++ Bird/Frist/Frist/Program.cs
@@ -0,0 +1,25 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: hp-pc
+ * Date: 2017/7/24
+ * Time: 22:24
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace Frist
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+			
+			// TODO: Implement Functionality Here
+			
+			Console.Write("Press any key to continue . . . ");
+			Console.ReadKey(true);
+		}
+	}
+}
