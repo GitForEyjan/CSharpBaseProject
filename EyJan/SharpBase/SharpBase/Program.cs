d774f4dc11eb7f071c8239f18459efaa85f8b75d
EyJan <GitByEyJan@gmail.com>
2017-07-29 22:50:28 +0800

    create project By EyJan

---
+++ EyJan/SharpBase/SharpBase/Program.cs
@@ -0,0 +1,25 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: PC
+ * Date: 2017/7/29
+ * Time: 22:42
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace SharpBase
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+			
+			// TODO: Implement Functionality Here
+			
+			Console.Write("Press any key to continue . . . ");
+			Console.ReadKey(true);
+		}
+	}
+}
